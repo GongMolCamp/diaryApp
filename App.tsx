22a824405644fcb7418dd458c193f64e1e31daf2
정예준 <yejun20020313@gmail.com>
2025-01-17 01:56:34 +0900

    yejun

--- App.tsx
+++ App.tsx
@@ -12,17 +12,16 @@ import {
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen';
 import MainTabNavigator from './navigators/MainTabNavigator';
 import DetailScreen from './screens/DiaryDetail';
 import SignUpScreen from './screens/SignUpScreen';
-
+import GlobalState from './contexts';
 type RootStackParamList = {
-  Login : undefined, 
-  Main : undefined,
-  Detail : undefined,
-  SignUp : undefined
+  Login: undefined,
+  Main: undefined,
+  Detail: undefined,
+  SignUp: undefined
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -30,14 +29,16 @@ const Stack = createStackNavigator<RootStackParamList>();
 function App(): React.JSX.Element {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Main" component={MainTabNavigator} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GlobalState>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="Main" component={MainTabNavigator} />
+          <Stack.Screen name="Detail" component={DetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GlobalState>
   );
 }
 
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -5,20 +5,116 @@
  * @format
  */
 
-import React from 'react';
+import React, { useContext, useEffect } from "react";
 import {
-  Button,
-  SafeAreaView,
+  FlatList,
+  Pressable,
+  StatusBar,
+  StyleSheet,
   Text,
   View,
-} from 'react-native';
+} from "react-native";
+
+import { GlobalContext } from "../contexts/index";
+//import Chatcomponent from "../components/Chatcomponent";
+import { socket } from "../utils";
 
 const ChatScreen: React.FC<{ route: any, navigation: any }> = ({ route, navigation }) => {
+  
+  const context = useContext(GlobalContext);
+
+  if (!context) {
+    throw new Error("GlobalContext is not provided.");
+  }
+
+  const {
+    currentUser,
+    allChatRooms,
+    setAllChatRooms,
+    setCurrentUser,
+  } = context;
+  
+  useEffect(() => {
+    socket.emit("getGroup", {
+      userId : 'testid'
+    });
+
+    socket.on("groupList", (groups : any) => {
+      console.log(groups ,'hhhhhhhhhhhhhhhhhhhhhhh');
+      setAllChatRooms(groups);
+    });
+  }, [socket]);
+
+
+  useEffect(() => {
+    if (currentUser.trim() === "") navigation.navigate("Homescreen");
+  }, [currentUser]);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Chat</Text>
+    <View style={styles.mainWrapper}>
+      <View style={styles.topContainer}>
+        <View style={styles.header}>
+          <Text style={styles.heading}>Welcome {currentUser}!</Text>
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        {allChatRooms && allChatRooms.length > 0 ? (
+          <FlatList
+            data={allChatRooms}
+            renderItem={({ item }) => <Text>채팅창</Text>}
+            keyExtractor={(item) => JSON.stringify(item)}
+          />
+        ) : null}
+      </View>
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  mainWrapper: {
+    backgroundColor: "#eee",
+    flex: 1,
+  },
+  topContainer: {
+    backgroundColor: "#fff",
+    height: 70,
+    width: "100%",
+    padding: 20,
+    justifyContent: "center",
+    marginBottom: 15,
+    flex: 0.3,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+  listContainer: {
+    flex: 3.4,
+    paddingHorizontal: 10,
+  },
+  bottomContainer: {
+    flex: 0.3,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#703efe",
+    padding: 12,
+    width: "100%",
+    elevation: 1,
+    borderRadius: 50,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
 
 export default ChatScreen;
